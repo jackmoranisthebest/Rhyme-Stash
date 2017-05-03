856de6fc1766f74dca1dc8f475806eccd0129111
Dilip Rao <DRao@dilips-macbook-air.local>
2017-05-03 17:53:49 -0500

    Hooked up rhyme buttons.

---
+++ Rhyme_Stash/RhymeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RhymeViewController.swift
+//  Rhyme_Stash
+//
+//  Created by Dilip Rao on 5/3/17.
+//  Copyright © 2017 The Bingo Boiz. All rights reserved.
+//
+
+import UIKit
+
+class RhymeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rhyme0(_ sender: UIButton) {
+    }
+    
+    @IBAction func rhyme1(_ sender: UIButton) {
+    }
+    
+    @IBAction func rhyme2(_ sender: UIButton) {
+    }
+    
+    @IBAction func rhyme3(_ sender: UIButton) {
+    }
+    
+    @IBAction func rhyme4(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func refreshSuggestions(_ sender: UIButton) {
+    }
+    
+    @IBAction func changeNumSyllables(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
